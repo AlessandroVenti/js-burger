813a8458dc19f2963cf141d55f4928ca85bc0264
Alessandro Venti <alessandro.venti@icloud.com>
2021-03-11 16:39:59 +0100

    var and for

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 console.log(`hello`);
 
+var calculateBtn = document.getElementById("calculate");
 
+calculateBtn.addEventListener('click', function() {
 
+    var checkboxCollection = document.getElementsByClassName("food");
+    
+    for (let i = 0; i < checkboxCollection.length; i++) {
+        var checkboxSingle = checkboxCollection[i];
+        
+    }
 
-
+});
